74b7ad5493266c7be97215a047381450b58ab58d
hun8729 <hun8729@handong.ac.kr>
2023-05-10 20:36:22 +0900

    Update book.c

--- book.c
+++ book.c
@@ -66,28 +66,19 @@ int selectMenu(){        //선택 메뉴
     int menu;
     printf("\n===도서 대출/반납 메뉴\n");
     printf("1. 대출 현황 조회\n");  //조회
-    printf("2. 도서 대출/n");       //추가
-    printf("3. 대출 현황 수정/n");  //수정
-    printf("4. 도서 반납/n");       //삭제
-    printf("5. 도서 이름 검색/n"); 
-    printf("6. 대출자 이름 검색/n");
-    printf("7. 대출 미납자/n");
-    printf("8. 오늘의 책/n");       //대출 중이 아닌 책들 중 하나 추천
+    printf("2. 도서 대출\n");       //추가
+    printf("3. 대출 현황 수정\n");  //수정
+    printf("4. 도서 반납\n");       //삭제
+    printf("5. 파일 저장하기\n");
+    printf("6. 도서 이름 검색\n"); 
+    printf("7. 대출자 이름 검색\n");
+    printf("8. 대출 미납자\n");
+    printf("9. 오늘의 책\n");       //대출 중이 아닌 책들 중 하나 추천
     printf("0. 종료 >> ");
     scanf("%d", &menu);
 
     return menu;
 }
-
-int selectNum(Book *b[], int count){ //선택한 메뉴 취소 or 번호 선택
-    int num;
-    listContact(b, count);
-    printf("번호는 (취소 : 0)? ");
-    scanf("%d", &num);
-
-    return num;
-}
-
 int listBook(Book *b[], int count){
     printf("\nNo    Name        StudentID       Book Name       Due date\n");
     printf("===============================================================\n");
@@ -103,10 +94,10 @@ int listBook(Book *b[], int count){
 
 void searchName(Book *b[], int count){
     int scnt=0;
-    char search[20];
+    int sID;
 
-    printf("검색할 본인 이름은? ");
-    scanf("%s", search);
+    printf("검색할 본인 학번은? ");
+    scanf("%d", &sID);
 
     printf("\nNo    Name        StudentID       Book Name       Due date\n");
     printf("===============================================================\n");
@@ -115,9 +106,9 @@ void searchName(Book *b[], int count){
         if(b[i] == NULL){
         continue;
         }
-        if(strstr(b[i]->name, search)){
+        if(b[i]->studnetID == sID){
         printf("%2d ", i+1);
-        readContact(*b[i]);
+        readBook(*b[i]);
         scnt++;
         }
     }
@@ -134,29 +125,35 @@ void searchBook(Book *b[], int count){
     printf("검색할 책의 이름은? ");
     scanf("%s", search);
 
-    printf("\nNo    Name        StudentID       Book Name       Due date\n");
-    printf("===============================================================\n");
+    printf("\nNo    Name        StudentID       Book Name       state \n");
+printf("===============================================================\n");
 
     for(int i=0; i<count; i++){
         if(b[i] == NULL){
         continue;
         }
         if(strstr(b[i]->bookName, search)){
+
         printf("%2d ", i+1);
-        readContact(*b[i]);
+        if(b[i]->returningstate==1){
+            printf("%s    %d   %s  반납 완료 \n",b[i]->name,b[i]->studnetID,b[i]->bookName);
+        }else{
+            printf("%s    %d   %s  대여 중 \n",b[i]->name,b[i]->studnetID,b[i]->bookName);
+            printf("반납 예정 일은 %hd월 %hd일입니다!\n",b[i]->endMonth,b[i]->endDay);
+        }
         scnt++;
         }
     }
     if(scnt == 0){
-        printf("\n===검색된 데이터 없음===\n");
+    printf("\n===검색된 데이터 없음===\n");
     }
     printf("\n");
 }
-
 void showOverdue(Book *b[], int count){
-    
+
 }
 
 void recommendBook(Book *b){
 
 }
+
