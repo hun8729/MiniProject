8ab2593faf8d57251972384986adf59d768e3a01
심성환 <oss_22000397@walab-HGU>
2023-05-12 13:42:13 +0900

    Add Livrary

--- manager.h
+++ manager.h
@@ -8,4 +8,4 @@ int listBook(Book *b[], Library *l[], int count);  //대여한 책 리스트 블
 void searchName(Book *b[], int count);    //본인 이름 검색
 void searchBook(Book *b[], int count);    //책 이름 검색
 void showOverdue(Book *b[], int count); //반납 날이 지난 책 조회
-void recommendBook(Book *b[], int count);    // 랜덤으로 대출 중이 아닌 책을 추천 해주는 기능
+void recommendBook(Library *l[], int count);    // 랜덤으로 대출 중이 아닌 책을 추천 해주는 기능
