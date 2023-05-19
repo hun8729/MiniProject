68e7b97a3c7261770046bf51b39f4c5f147db7e8
심성환 <oss_22000397@walab-HGU>
2023-05-19 17:14:46 +0900

    Update recommand

--- manager.h
+++ manager.h
@@ -8,4 +8,4 @@ void listBook(Book *b[], int count);  //대여한 책 리스트 블러오기
 void searchName(Book *b[], int count);    //본인 이름 검색
 void searchBook(Book *b[], int count);    //책 이름 검색
 void showOverdue(Book *b[], int count); //반납 날이 지난 책 조회
-void recommendBook(Library *l[], int count);    // 랜덤으로 대출 중이 아닌 책을 추천 해주는 기능
+int recommendBook(Library *l[]);    // 랜덤으로 대출 중이 아닌 책을 추천 해주는 기능
