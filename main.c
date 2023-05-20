75ea073397a80bf1802cc8f8a1575e4476e865b2
심성환 <oss_22000397@walab-HGU>
2023-05-20 16:07:21 +0900

    main

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main(void){
       }
     else if(menu == 2){
       b[index] = (Book *)malloc(sizeof(Book));
-      count += createBook(b[index]);
+      count += createBook(b[index], l);
       index++;
     }
     else if(menu == 3){
